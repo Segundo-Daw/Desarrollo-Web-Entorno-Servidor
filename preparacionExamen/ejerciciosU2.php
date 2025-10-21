e55b6ea8f5d8e0f7619c2cf478f308fea95575ed
CarolinaRuescas <ruescas17@gmail.com>
2025-10-22 00:26:05 +0200

    .

--- preparacionExamen/ejerciciosU2.php
+++ preparacionExamen/ejerciciosU2.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ejercicios 2 y 3</title>
+    <link rel="stylesheet" href="tablasMultiplicarEstilos.css">
+
 </head>
 <body>
     <h2>Ejercicio 1</h2>
@@ -203,12 +205,224 @@
     Escribe un programa que dado un número contenido en la variable numero, muestre el factorial del mismo. El factorial es el producto de los números positivos desde 1 hasta dicho número. Es decir, el factorial de 5 es 1x2x3x4x5 = 120.
     */
     $numero = 5;
-    for ($i = 1; $i < $numero; $i++){
-        $numero *= $i;
+    $factorial =1;
+    for ($i = 1; $i < $numero; $i++){   //para que salga el resultado hay que poner <= , si ponemos solo < recorre hasta 4 y da 24
+        $factorial *= $i;
     }
 
-    echo "<p>El factorial es $numero.</p>"
+    echo "<p>El factorial es $factorial.</p>"
     ?>
+
+    <h2>Ejercicio 13</h2>
+    <?php
+    /*
+    Dadas un par de variables base y exponente, muestra por pantalla el resultado de elevar la base al exponente. Tienes que utilizar un bucle for para ello. No puedes usar el operador ** ni el método pow().
+    */
+    $base = 4;
+    $exponente = 3;
+    $potencia = 1;  //importante que inicie n 1 porque s donde se van a ir guardando las iteraciones
+
+    for($i = 0; $i < $exponente ; $i++ ){
+        $potencia *= $base;
+        
+    }
+    echo "<p>El resultado de tener como base $base y de exponente $exponente es $potencia .<p>";
+
+    ?>
+
+    <h2>Ejercicio 14</h2>
+    <?php
+    /*
+    Reescribe el ejercicio anterior utilizando un bucle while.
+    */
+    $base = 4;
+    $exponente = 3;
+    $potencia = 1;  //acumulador
+    $num =0;   // contador bucle
+
+    while($num < $exponente){
+        $potencia *= $base;
+        $num++; //incrementa el numero en uno para que se cuente una vuelta del bucle
+    }
+    echo "<p>Si elevamos $base a $exponente el resultado es $potencia. </p>"
+    ?>
+
+    <h2>Ejercicio 15</h2>
+    <?php
+    /*
+    Declara una variable llamada numero. Muestra en una tabla (con bordes) la tabla de multiplicar de dicho número. La primera fila de la tabla es de títulos. Por ejemplo, si numero=7:
+    */
+    $numero = 2;
+
+    echo "<table border =1>
+            <tr>
+                <td>a</td>
+                <td>b</td>
+                <td>resultado</td>
+            </tr>";
+
+    for($i = 0; $i <= 10; $i++){
+        echo "<tr>
+                <td>$numero </td>
+                <td>$i </td>
+                <td>" . $numero * $i . "</td>
+            </tr>";
+    }
+
+    echo "</table>";
+
+
+    ?>
+
+    <h2>Ejercicio 16</h2>
+    <?php
+    /*
+    Crea un programa que imprima 20 cifras de la secuencia Fibonacci, separados por comas, en un mismo párrafo todos ellos.
+    Fibonacci es una secuencia de números donde cada termino es la suma de los dos anteriores
+    */
+    $valorAnterior = 0;  //numero actual que se va a imprimir
+    $valorSiguiente = 1;  //numero siguiente de la secuencia 
+
+    echo "<p>";
+    for($i = 0; $i < 20 ; $i++){
+        echo "$valorAnterior, ";
+        $aux = $valorSiguiente;  // se declara una nueva variable temporal para que se guarde el nuevo valor del numero 
+        $valorSiguiente += $valorAnterior;
+        $valorAnterior = $aux;
+    }
+    echo "</p>";
+    ?>
+
+    <h2>Ejercicio 17</h2>
+    <?php
+    /*
+    Crea un programa que dados los valores de dos variables, filas y columnas, imprima una matriz con tantas filas y columnas como digan dichas variables con el símbolo *. Por ejemplo, si filas=3 y columnas=5, la salida debería ser:
+
+    * * * * *
+    * * * * *
+    * * * * *
+
+    */
+
+    $filas = 3;
+    $columnas = 5;
+
+    for ($i = 0; $i < $filas; $i++){
+        for ($j = 0; $j < $columnas; $j++){
+            echo "* ";
+        }
+        echo "<br>";    //poner este br bien para que se bajen las filas en cada vuelta del bucle for
+    }
+    ?>
+
+    <h2>Ejercicio 18</h2>
+    <?php
+    /*
+    Crea un programa parecido al anterior, pero que haga una pirámide. En este caso solo tenemos la variable filas. Si filas=5, la salida será:
+
+    *
+    *  *
+    *  *  *
+    *  *  *  *
+    *  *  *  *  *
+
+    */
+    $filas = 5;
+    $columnas = 1;
+
+    for ($i = 0; $i < $filas; $i++){
+        for ($j = 0; $j < $columnas; $j++){
+            echo "* ";
+        }
+        $columnas++;
+        echo "<br>";    
+    }
+
+    ?>
+
+    <h2>Ejercicio 19</h2>
+    <?php
+    /*
+    Dado un valor en la variable numero, imprime una pirámide como en el ejemplo. Si numero=5, la salida será:
+    1
+    1  2
+    1  2  3
+    1  2  3  4
+    1  2  3  4  5
+    */
+
+    $filas = 5;
+    $columnas = 1;
+
+    for ($i = 0; $i < $filas; $i++){
+        for ( $j = 0; $j < $columnas; $j++){
+            echo $j + 1  . " ";
+        }
+        $columnas++;
+        echo "<br>";
+    }
+
+    ?>
+
+    <h2>Ejercicio 20</h2>
+    <!-- Crea un programa en PHP en un fichero nuevo llamado tablasMultiplicar.php, que llevará asociado una hoja de estilos css llamada tablasMultiplicarEstilos.css. Genera, a través de bucles anidados, una tabla lo más parecida a esta (colores, separaciones entre celdas, etc).-->
+
+    <table>
+        <?php
+        for ( $i = 0; $i <= 10; $i++){
+            echo "<tr>";
+            for ($j = 0; $j <= 10; $j++){
+                if ($i == 0){
+                    if ( $j== 0){
+                        echo "<th class='thx'>X</th>";
+                    }else{
+                        echo "<th class = 'thnumber'>". $j - 1 . "</th>";
+                    }
+                }else{
+                    if ($j == 0){
+                        echo "<td class = 'tdnumber'>" . $i - 1 . "</td>";
+                    }else{
+                        $multiplicacion = ($i - 1) * ($j - 1 );
+                        echo "<td class ='normal'>$multiplicacion</td>";
+                    }
+                }
+
+            }
+            echo "</tr>";
+        }
+
+        ?>
+    </table>
+
+    <h2>Ejercicio 21</h2>
+    <?php
+    /*
+    Dada una variable llamada cadena, imprime por pantalla la longitud de dicha cadena.
+    A continuaicón, súmale 1 a esa cadena (utilizando ++), ¿qué ocurre?
+    Pon espacios delante y detrás e investiga un método que elimine estos espacios (esto será muy útil en la validación de formularios web, pues es común que la gente meta un espacio después de su email, por ejemplo).
+    */
+
+    $cadena = "php";
+    $cadena++;
+    var_dump($cadena);  // sale phq, es decir, a la ultima letra "se le añade 1", pasa a la siguiente letra del abecedario
+
+
+    $cadena = " hola ";
+    var_dump($cadena);  // 6 porque son 4 letras mas dos espacios
+    //Para quitar espacios delante y detrás, función:
+    $cadena = trim($cadena);
+    var_dump($cadena); // 4 sale esto porque quita los espacios
+
+    ?>
+
+
+
+
+
+
+
+
+    
 
 
 
---
+++ preparacionExamen/ejerciciosU3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicios Arrays</title>
+</head>
+<body>
+    <h2>Ejercicio 1</h2>
+    <?php
+    /*
+    Crea el array aleatorios, que contenga 20 números aleatorios entre 10 y 50. Muestra por pantalla el contenido de dicho array dentro de un párrrafo (<p>) y con los números separados por comas.
+    Pista: investiga una función de PHP que devuelve números aleatorios.    
+    */
+    
+
+    ?>
+    
+</body>
+</html>
