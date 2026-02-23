505e20323a96470512607c50b4d8fe06c902bf5e
CarolinaRuescas <ruescas17@gmail.com>
2026-02-23 12:36:12 +0100

    .

---
+++ Laravel/apirest_CarolinaR/resources/views/components/header.blade.php
@@ -0,0 +1,11 @@
+<header>
+    <h1 class="text-center fw-bold display-3 py-4">Tierno News</h1>
+    <nav  class="navbar navbar-expand-lg navbar-light bg-dark">
+        <ul class="navbar-nav">
+            <li class="nav-item active"><a class="nav-link text-white" href="{{ route('') }}">Inicio</a></li>
+            <li class="nav-item active"><a class="nav-link text-white" href="{{ route('') }}">Crear</a></li>
+            <li class="nav-item active"><a class="nav-link text-white" href="{{ route('') }}">Articulos</a></li>
+            <li class="nav-item active"><a class="nav-link text-white" href="{{ route('') }}">Crear</a></li>
+        </ul>
+    </nav>
+</header>
